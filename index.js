66a747d7a5f6b0d0413d425bad5232e3877d9ed2
paltovkletku <144653281+paltovkletku@users.noreply.github.com>
2026-06-01 12:09:33 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,27 @@
+import express from 'express';
+import https from 'https';
+
+const app = express();
+
+app.get('/login', (req, res) => {
+  res.send('annaborisevich');
+});
+
+app.get('/id/:n', (req, res) => {
+  https.get(`https://nd.kodaktor.ru/users/${req.params.n}`, (response) => {
+    let data = '';
+    response.on('data', (chunk) => { data += chunk; });
+    response.on('end', () => {
+      try {
+        const json = JSON.parse(data);
+        res.send(json.login);
+      } catch (e) {
+        res.status(500).send('parse error');
+      }
+    });
+  }).on('error', (err) => {
+    res.status(500).send(err.message);
+  });
+});
+
+app.listen(process.env.PORT || 3000);
