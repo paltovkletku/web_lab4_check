ed79dd5a79e6f0b52688ca0dcb517f21b2e07005
paltovkletku <144653281+paltovkletku@users.noreply.github.com>
2026-06-01 12:26:36 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 import express from 'express';
-import https from 'https';
 
 const app = express();
 
@@ -7,21 +6,40 @@ app.get('/login', (req, res) => {
   res.send('annaborisevich');
 });
 
-app.get('/id/:n', (req, res) => {
-  https.get(`https://nd.kodaktor.ru/users/${req.params.n}`, (response) => {
-    let data = '';
-    response.on('data', (chunk) => { data += chunk; });
-    response.on('end', () => {
-      try {
-        const json = JSON.parse(data);
-        res.send(json.login);
-      } catch (e) {
-        res.status(500).send('parse error');
-      }
-    });
-  }).on('error', (err) => {
-    res.status(500).send(err.message);
-  });
+app.get('/id/:n', async (req, res) => {
+  const response = await fetch(`https://nd.kodaktor.ru/users/${req.params.n}`);
+  const json = await response.json();
+  res.send(json.login);
 });
 
 app.listen(process.env.PORT || 3000);
+
+
+
+// import express from 'express';
+// import https from 'https';
+
+// const app = express();
+
+// app.get('/login', (req, res) => {
+//   res.send('annaborisevich');
+// });
+
+// app.get('/id/:n', (req, res) => {
+//   https.get(`https://nd.kodaktor.ru/users/${req.params.n}`, (response) => {
+//     let data = '';
+//     response.on('data', (chunk) => { data += chunk; });
+//     response.on('end', () => {
+//       try {
+//         const json = JSON.parse(data);
+//         res.send(json.login);
+//       } catch (e) {
+//         res.status(500).send('parse error');
+//       }
+//     });
+//   }).on('error', (err) => {
+//     res.status(500).send(err.message);
+//   });
+// });
+
+// app.listen(process.env.PORT || 3000);
